3b8046fd19bf69e4948e26668218ca38c1653537
preeth <preeth@pop-os.localdomain>
2022-03-11 00:36:53 +0530

    second oush

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
-from flask import Flask, render_template
+from crypt import methods
+from email import message
+from flask import Flask, render_template,request
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+import nltk
+nltk.download('vader_lexicon')
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/',methods=["GET","POST"])
 def main():
+    if request.method == "POST":
+        inp = request.method
+        sid = SentimentIntensityAnalyzer()
+        score = sid.polarity_scores(inp)
+        
+        if score["neg"] !=0:
+            return render_template('home.html', message="NEGETIVE")
+        else:
+            return render_template('home.html', message="POSITIVE")
     return render_template('home.html')
